9a8628f23cbc8a1699477a9a5cb9fc3ec6df864d
h1code2 <huangyongjin@surfilter.com>
2023-10-19 11:19:37 +0800

    update

--- install_docker.sh
+++ install_docker.sh
@@ -23,11 +23,11 @@ sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 sudo systemctl start docker
 sudo systemctl enable docker
 
-然后，在目标机器上，你可以使用以下命令来下载并运行该脚本：
+# 然后，在目标机器上，你可以使用以下命令来下载并运行该脚本：
 
-wget -O install_docker.sh https://raw.githubusercontent.com/{YOUR_USERNAME}/repository/main/install_docker.sh
-chmod +x install_docker.sh
-sudo ./install_docker.sh
+# wget -O install_docker.sh https://raw.githubusercontent.com/{YOUR_USERNAME}/repository/main/install_docker.sh
+# chmod +x install_docker.sh
+# sudo ./install_docker.sh
 
 # use
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/h1code2/install_script/main/install_docker.sh)"
